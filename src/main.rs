0a96b64296ace14ace15207a166cf07ec5f40c60
bonqus <mathias_b2@hotmail.com>
2024-01-28 11:39:02 +0100

    Day 1 solution

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use std::env;
+use std::fs::read_to_string;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let file_path = &args[1];
+    let file = read_to_string(file_path).expect("No file");
+
+    let result = find_calibration_sum(&file);
+    match result {
+        Some(result) => println!("{}", result),
+        None => println!("Something went wrong!"),
+    }
+}
+
+fn find_calibration_sum(file: &str) -> Option<u32> {
+    file.lines().map(find_calibration).sum()
+}
+
+fn find_calibration(line: &str) -> Option<u32> {
+    let first = find_digit(line.chars())?;
+    let last = find_digit(line.chars().rev())?;
+    Some(first * 10 + last)
+}
+
+
+fn find_digit(vals: impl Iterator<Item = char>) -> Option<u32> {
+    vals.into_iter().find_map(|c | c.to_digit(10))
 }
